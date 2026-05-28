9a0ea84c9c55a83c4b743bb0e8e7759d177ea793
Sune Engtorp <suneengtorp@live.dk>
2026-05-28 21:11:39 +0200

    chore: add fixed ports to aspire resources

--- apphost/AppHost.cs
+++ apphost/AppHost.cs
@@ -17,7 +17,7 @@ builder.AddDockerComposeEnvironment("wyb")
            file.Name = "wyb";
        });
 
-var postgres = builder.AddPostgres("postgres")
+var postgres = builder.AddPostgres("postgres", port: 5433)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithContainerName("wyb-postgres")
@@ -28,14 +28,15 @@ var postgres = builder.AddPostgres("postgres")
 
 var ledgerDb = postgres.AddDatabase("ledger-db");
 
-var rabbit = builder.AddRabbitMQ("rabbit", rabbitmqUsername, rabbitmqPassword)
-    .WithManagementPlugin()
+var rabbit = builder.AddRabbitMQ("rabbit", rabbitmqUsername, rabbitmqPassword, port: 5672)
+    .WithManagementPlugin(15672)
     .WithContainerName("wyb-rabbit")
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
 // .NET service: ledger
 var ledger = builder.AddProject<Projects.Wyb_Ledger>("ledger")
+    .WithEndpoint("http", e => e.Port = 5100)
     .WithReference(ledgerDb)
     .WithReference(rabbit)
     .WaitFor(ledgerDb)
@@ -43,7 +44,7 @@ var ledger = builder.AddProject<Projects.Wyb_Ledger>("ledger")
 
 // Go service: ingest
 var ingest = builder.AddGolangApp("ingest", "../services/ingest")
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpEndpoint(port: 5200, env: "PORT")
     .WithOtlpExporter()
     .WithReference(rabbit)
     .WaitFor(rabbit)
@@ -55,14 +56,14 @@ var rulesDb = postgres.AddDatabase("rules-db");
 
 var rules = builder.AddUvicornApp("rules", "../services/rules", "rules.main:app")
     .WithUv()
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpEndpoint(port: 5300, env: "PORT")
     .WithReference(rulesDb)
     .WaitFor(rulesDb)
     .WithEnvironment("PYTHONUNBUFFERED", "1");
 
 var categorize = builder.AddUvicornApp("categorize", "../services/categorize", "categorize.main:app")
     .WithUv()
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpEndpoint(port: 5400, env: "PORT")
     .WithReference(rabbit)
     .WithReference(rules)
     .WaitFor(rabbit)
@@ -76,6 +77,7 @@ builder.AddUvicornApp("detect", "../services/detect", "detect.main:app")
 
 // SvelteKit frontend
 builder.AddViteApp("web", "../services/web")
+    .WithEndpoint("http", e => e.Port = 5173)
     .WithReference(ledger)
     .WithReference(rules)
     .WaitFor(ledger)
